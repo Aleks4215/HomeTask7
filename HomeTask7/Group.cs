68f4dbe17ae5c66b6b6e68be2faf9ae3c2f4600c
Aleksey Evmenkov <aleksey4215@gmail.com>
2020-04-07 16:00:02 +0300

    dev

---
+++ HomeTask7/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Group
+{
+    List<Student> students = new List<Student>();
+    Student student = new Student();
+
+    private void GetRatingsForAllStudents()
+    {
+        if(students.Count == 0)
+        {
+            Console.WriteLine("Нет добавленных студентов");
+        } else
+        {
+            for(int i = 0; i < students.Count -1; i++)
+            {
+                students[i].PrintAllRatingsOfStudent();
+            }
+        }
+    }
+}
---
+++ HomeTask7/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+
+    private string firstName, lastName;
+    private List<int> ratings = new List<int>();
+
+    private void AddRating(int rating) => ratings.Add(rating);
+    private decimal GetAverageRating() => (decimal)ratings.Average();
+
+    private List<int> PrintAllRatingsOfStudent()
+    {
+        if (ratings.Count == 0)
+        {
+            Console.WriteLine("Список оценок пуст");
+            return null;
+        }
+        else
+        {
+            return ratings;
+        }
+
+    }
+}
+
